e5de225ef7ff221f7530d6729b2c4ea969d3b98a
Tavote29 <86198332+Tavote29@users.noreply.github.com>
2023-02-13 18:11:29 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('light');
+    
+    btnSwitch.classList.toggle('active');
+});
