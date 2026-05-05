40decd39af143c3a917efd9840fbf50981e4ab9b
Tavote29 <gustavo_angel2000@hotmail.com>
2026-05-05 13:36:56 -0600

    Se integra un carrusel para mejorar la visualización de los proyectos, tambien se cambia la forma de cargar algunos elementos de la pagina, ahora se generan de manera dinámica, lo cual reduce código y se vuelve más escalable. Se eliminan archivos que no eran necesarios y se actualiza el CV a la ultima versión

---
+++ js/datos.js
@@ -0,0 +1,89 @@
+const exp = [
+     {
+        empresa: "STEFANINI S.A DE C.V",
+        imagen: "images/projects/Stefanini_Group_Logo.png",
+        descripcion: "Una empresa global con 38 años de experiencia en el mercado, que ofrece una sólida selección de servicios como servicios financieros, automatización, nube, Internet de las cosas (IoT) y experiencia de usuario (UX)",
+        contrato: "Mayo - Noviembre 2025",
+        actividades: [
+            "Colaboré con BANAMEX, creando scripts de instalación y configuración de colas (queues) en IBM MQ y desarrollé microservicios SpringBoot adaptandose a los requerimientos del cliente, mejorando la integración entre sistemas.",
+            "Participé de manera activa como QA Tester garantizando la calidad del software mediante la ejecución de pruebas automatizadas usando Postman y plasmandolos en reportes automatizados usando Karate Framework y Newman con Node JS"
+        ]
+    },
+    {
+        empresa: "TASF S.A de C.V",
+        imagen: "images/projects/tasf.png",
+        descripcion: "Empresa mexicana con 10 años de experiencia. Especialistas en desarrollo de Software Financiero.",
+        contrato: "Agosto 2022 - Octubre 2024",
+        actividades: [
+            "Desarrollé un módulo capaz de interpretar las tramas de buró de crédito para el área de Soporte Técnico del departamento de Desarrollo de Software, facilitando su lectura, reduciendo significativamente el tiempo de toma de decisiones y detección de errores.",
+            "Desarrollé webservices SOAP y APIs REST desarrollados con JAVA y SPRINGBOOT.",
+            "Diseñé interfaces de usuario para aplicaciones de escritorio usando WPF y .NET e implementé los WS y API REST desarrollados.",
+            "Elaboré vistas, procedimientos almacenados y consultas avanzadas usando SQL Server.",
+            "Diseñar, desarrollar y generar reportes PDF usando JASPERSOFT.",
+            "Mantenimiento preventivo y correctivo a las funciones de los módulos desarrollados del sistema web adaptándose a las necesidades de cada cliente."
+        ]
+    }
+];
+
+const lenguages = [
+    {
+        lenguage: "Java (Springboot)",
+        logos: [
+            "images/logos/java.png",
+            "images/logos/spring-boot.png"   
+        ]
+    },
+    {
+        lenguage: "C# (.NET)",
+        logos:[
+            "images/logos/csharp.png",
+            "images/logos/NET-Framework-Symbol.png"
+        ]
+    },
+    {
+        lenguage: "SQL (SQL Server & MySQL)",
+        logos:[
+            "images/logos/mysql.png",
+            "images/logos/sqlserver.png"
+        ]
+    },
+    {
+        lenguage: "HTML5, CSS3 & JAVASCRIPT",
+        logos:[
+            "images/logos/html.png",
+            "images/logos/css.png",
+            "images/logos/logo-javascript.png"
+        ]
+    },
+    {
+        lenguage: "PHP (Laravel)",
+        logos: [
+            "images/logos/php.png",
+            "images/logos/laravel.png"
+        ]
+    }
+];
+
+const projects = [
+    {
+        name: "API LIGA_MX",
+        hypervincule: "https://github.com/Tavote29/API_LigaMx",
+        description: "Una API que simula al de la página oficial de la Liga MX aplicando las tecnologías que aprendí lo largo de mi carrera profesional",
+        image: "images/projects/API_LigaMX.png",
+        text: "Una API basado en el sistema actual de la liga MX en las que se puede hacer operaciones CRUD y manejo de estadísticas como tabla de posiciones, tabla de goleo, etc. Elaborado con Springboot 4, usando SQL Server como gestor de base de datos, Spring Security para manejo de roles con autenticación JWT, implementando Swagger para la documentación y JUnit con Mockito para la elaboración de pruebas unitarias."
+    },
+    {
+        name: "Mi Portfolio",
+        hypervincule: "https://github.com/Tavote29/Tavote29.github.io",
+        description: "Mi propia página web donde muestro mi carrera profesional, habilidades y proyectos que he realizado",
+        image: "images/projects/portfolio.PNG",
+        text: "Una página web personal en la que se puede encontrar más detalles de mi carrera profesional. Hecho principalmente con Bootstrap v3.3.4 con HTML 5, CSS y Javascript"
+    },
+    {
+        name: "MINECODE",
+        hypervincule: "https://github.com/Tavote29/Compilador",
+        description: "Un compilador para el lenguaje Java",
+        image: "images/projects/Minecode.png",
+        text: "Un programa que formó parte de un proyecto escolar, diseñado en C# con la librería Irony hecho para el lenguaje Java y muestre los analizadores léxico, sintáctico y semántico."
+    }
+];
--- js/main.js
+++ js/main.js
@@ -5,3 +5,107 @@ btnSwitch.addEventListener('click', () => {
     
     btnSwitch.classList.toggle('active');
 });
+
+function initTooltip(){
+    $('[data-toggle="tooltip"]').tooltip();
+}
+
+$(document).ready(function(){
+    initTooltip();
+})
+
+const container = document.getElementById("exp-container");
+
+exp.forEach(exp => {
+    const item = document.createElement('div');
+    item.className = 'item row';
+
+    item.innerHTML = `
+        <a class="col-md-4 col-sm-4 col-xs-12" >
+        <img class="img-responsive project-image" src="${exp.imagen}" />
+        </a>
+
+    <div class="desc col-md-8 col-sm-8 col-xs-12">
+      <h3 class="title">${exp.empresa}</h3>
+      <p class="summary">${exp.contrato}<p>
+
+      <p class="summary">${exp.descripcion}</p>
+      <div class="desc text-left">
+        <ul>
+          ${exp.actividades.map(act => `<li>${act}</li>`).join('')}
+        </ul>
+      </div>
+    </div>   
+    `
+    container.appendChild(item);
+});
+
+const stack = document.getElementById("technologic-stack");
+lenguages.forEach(dat => {
+    const item = document.createElement('div');
+    item.className= 'item';
+
+    item.innerHTML = `
+        <div class="item">
+            <h3 class="level-title">${dat.lenguage}<span class="level-label" data-toggle="tooltip" data-placement="left" data-animation="true" title="You can use the tooltip to add more info..."></span></h3>
+             ${dat.logos.map(logo => 
+            `<img class="logos" src="${logo}" alt="${exp.lenguage}">`
+        ).join('')}
+    `
+
+    stack.appendChild(item);
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const carousel = document.getElementById("carousel_elements");
+    const indicators = document.querySelector('#carrusel .carousel-indicators');
+
+    carousel.innerHTML= '';
+    indicators.innerHTML = '';
+
+    projects.forEach((dat,index) => {
+
+    const item = document.createElement('div')
+    item.className = 'item featured text-center';
+
+    if(index == 0){
+        item.classList.add('active');
+    }
+
+    item.innerHTML = `
+
+            <h3 class="title">
+                <a href="${dat.hypervincule}" data-toggle="tooltip" title="Ir al repositorio" target="_blank">
+                    ${dat.name}
+                </a>
+            </h3>
+
+            <p class="summary"> ${dat.description}
+            </p>
+            <div class="featured-image">
+                <img class="img-responsive project-image"  src="${dat.image}">
+            </div>
+
+            <div class="desc text-justify">
+                <p>
+                    ${dat.text}
+                </p>
+            </div>
+    `
+    carousel.appendChild(item)
+
+    const li = document.createElement('li');
+    
+    li.setAttribute('data-target', '#carrusel');
+    li.setAttribute('data-slide-to', index);
+
+    if(index == 0){
+        li.classList.add('active');
+    }
+
+    indicators.appendChild(li);
+    });
+
+
+})
