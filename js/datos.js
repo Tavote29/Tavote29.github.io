f5796e33cd7dc5d6579032eb63c06df9deb87342
Tavote29 <gustavo_angel2000@hotmail.com>
2026-05-08 19:44:07 -0600

    A bilingual function is created: Now, the text switches from English to Spanish and viceversa

--- js/datos.js
+++ js/datos.js
@@ -1,89 +1,226 @@
-const exp = [
-     {
-        empresa: "STEFANINI S.A DE C.V",
-        imagen: "images/projects/Stefanini_Group_Logo.png",
-        descripcion: "Una empresa global con 38 años de experiencia en el mercado, que ofrece una sólida selección de servicios como servicios financieros, automatización, nube, Internet de las cosas (IoT) y experiencia de usuario (UX)",
-        contrato: "Mayo - Noviembre 2025",
-        actividades: [
-            "Colaboré con BANAMEX, creando scripts de instalación y configuración de colas (queues) en IBM MQ y desarrollé microservicios SpringBoot adaptandose a los requerimientos del cliente, mejorando la integración entre sistemas.",
-            "Participé de manera activa como QA Tester garantizando la calidad del software mediante la ejecución de pruebas automatizadas usando Postman y plasmandolos en reportes automatizados usando Karate Framework y Newman con Node JS"
-        ]
+const translations = {
+    es: {
+        static: {
+            title: "Desarrollador Backend",
+            about_me: "Sobre mi",
+            profile_description: "Tengo mucha motivación para seguir perfeccionando habilidades y crecer profesionalmente. Soy una persona proactiva y autodidacta que constantemente aprende algo nuevo para aportar con mi equipo. Me entusiasma el desarrollo backend, bases de datos y me fascina la ciencia de datos y todo lo que tenga que ver en ello.",
+            persistent: "Perseverante",
+            self_taught:"Autodidacta",
+            proactive: "Proactivo",
+            cooperative: "Trabajo en equipo",
+            autocritic: "Autocrítico",
+            expertise: "Experiencia Profesional",
+            projects: "Proyectos personales",
+            mexico: "México",
+            technologies: "Lenguajes y tecnologías",
+            education: "Educación",
+            engineering: "Ingeniería en Sistemas Computacionales",
+            technician: "Técnico en Soporte y Mantenimiento de Equipo de Computo",
+            languages: "Idiomas",
+            spanish: "Español:",
+            spanish_level : "Idioma nativo",
+            english: "Inglés:",
+            english_level : "Intermedio (B1)",
+            competitions: "Concursos"
+        },
+        exp : [
+            {
+                empresa: "STEFANINI S.A DE C.V",
+                imagen: "images/projects/Stefanini_Group_Logo.png",
+                descripcion: "Una empresa global con 38 años de experiencia en el mercado, que ofrece una sólida selección de servicios como servicios financieros, automatización, nube, Internet de las cosas (IoT) y experiencia de usuario (UX)",
+                contrato: "Mayo - Noviembre 2025",
+                actividades: [
+                    "Colaboré con BANAMEX, creando scripts de instalación y configuración de colas (queues) en IBM MQ y desarrollé microservicios SpringBoot adaptandose a los requerimientos del cliente, mejorando la integración entre sistemas.",
+                    "Participé de manera activa como QA Tester garantizando la calidad del software mediante la ejecución de pruebas automatizadas usando Postman y plasmandolos en reportes automatizados usando Karate Framework y Newman con Node JS"
+                ]
+            },
+            {
+                empresa: "TASF S.A de C.V",
+                imagen: "images/projects/tasf.png",
+                descripcion: "Empresa mexicana con 10 años de experiencia. Especialistas en desarrollo de Software Financiero.",
+                contrato: "Agosto 2022 - Octubre 2024",
+                actividades: [
+                    "Desarrollé un módulo capaz de interpretar las tramas de buró de crédito para el área de Soporte Técnico del departamento de Desarrollo de Software, facilitando su lectura, reduciendo significativamente el tiempo de toma de decisiones y detección de errores.",
+                    "Desarrollé webservices SOAP y APIs REST desarrollados con JAVA y SPRINGBOOT.",
+                    "Diseñé interfaces de usuario para aplicaciones de escritorio usando WPF y .NET e implementé los WS y API REST desarrollados.",
+                    "Elaboré vistas, procedimientos almacenados y consultas avanzadas usando SQL Server.",
+                    "Diseñar, desarrollar y generar reportes PDF usando JASPERSOFT.",
+                    "Mantenimiento preventivo y correctivo a las funciones de los módulos desarrollados del sistema web adaptándose a las necesidades de cada cliente."
+                ]
+            }
+        ],
+        projects : [
+            {
+                name: "API LIGA_MX",
+                hypervincule: "https://github.com/Tavote29/API_LigaMx",
+                description: "Una API que simula al de la página oficial de la Liga MX aplicando las tecnologías que aprendí lo largo de mi carrera profesional",
+                image: "images/projects/API_LigaMX.png",
+                text: "Una API basado en el sistema actual de la liga MX en las que se puede hacer operaciones CRUD y manejo de estadísticas como tabla de posiciones, tabla de goleo, etc. Elaborado con Springboot 4, usando SQL Server como gestor de base de datos, Spring Security para manejo de roles con autenticación JWT, implementando Swagger para la documentación y JUnit con Mockito para la elaboración de pruebas unitarias."
+            },
+            {
+                name: "Mi Portfolio",
+                hypervincule: "https://github.com/Tavote29/Tavote29.github.io",
+                description: "Mi propia página web donde muestro mi carrera profesional, habilidades y proyectos que he realizado",
+                image: "images/projects/portfolio.PNG",
+                text: "Una página web personal en la que se puede encontrar más detalles de mi carrera profesional. Hecho principalmente con Bootstrap v3.3.4 con HTML 5, CSS y Javascript"
+            },
+            {
+                name: "MINECODE",
+                hypervincule: "https://github.com/Tavote29/Compilador",
+                description: "Un compilador para el lenguaje Java",
+                image: "images/projects/Minecode.png",
+                text: "Un programa que formó parte de un proyecto escolar, diseñado en C# con la librería Irony hecho para el lenguaje Java y muestre los analizadores léxico, sintáctico y semántico."
+            }
+        ],
+        lenguages : [
+            {
+                lenguage: "Java (Springboot)",
+                logos: [
+                    "images/logos/java.png",
+                    "images/logos/spring-boot.png"   
+                ]
+            },
+            {
+                lenguage: "C# (.NET)",
+                logos:[
+                    "images/logos/csharp.png",
+                    "images/logos/NET-Framework-Symbol.png"
+                ]
+            },
+            {
+                lenguage: "SQL (SQL Server & MySQL)",
+                logos:[
+                    "images/logos/mysql.png",
+                    "images/logos/sqlserver.png"
+                ]
+            },
+            {
+                lenguage: "HTML5, CSS3 & JAVASCRIPT",
+                logos:[
+                    "images/logos/html.png",
+                    "images/logos/css.png",
+                    "images/logos/logo-javascript.png"
+                ]
+            },
+        {
+            lenguage: "PHP (Laravel)",
+            logos: [
+                "images/logos/php.png",
+                "images/logos/laravel.png"
+            ]
+        }
+]
     },
-    {
-        empresa: "TASF S.A de C.V",
-        imagen: "images/projects/tasf.png",
-        descripcion: "Empresa mexicana con 10 años de experiencia. Especialistas en desarrollo de Software Financiero.",
-        contrato: "Agosto 2022 - Octubre 2024",
-        actividades: [
-            "Desarrollé un módulo capaz de interpretar las tramas de buró de crédito para el área de Soporte Técnico del departamento de Desarrollo de Software, facilitando su lectura, reduciendo significativamente el tiempo de toma de decisiones y detección de errores.",
-            "Desarrollé webservices SOAP y APIs REST desarrollados con JAVA y SPRINGBOOT.",
-            "Diseñé interfaces de usuario para aplicaciones de escritorio usando WPF y .NET e implementé los WS y API REST desarrollados.",
-            "Elaboré vistas, procedimientos almacenados y consultas avanzadas usando SQL Server.",
-            "Diseñar, desarrollar y generar reportes PDF usando JASPERSOFT.",
-            "Mantenimiento preventivo y correctivo a las funciones de los módulos desarrollados del sistema web adaptándose a las necesidades de cada cliente."
+    en: {
+        static:{
+            title: "Backend Developer",
+            about_me: "About Me",
+            profile_description: "I am highly motivated to continue refining my skills and growing professionally. I am a proactive and self-taught individual who is constantly learning something new to contribute to my team. I am enthusiastic about backend development and databases, and I am fascinated by data science and everything related to it.",
+            persistent: "Persistent",
+            self_taught:"Self-taught",
+            proactive: "Proactive",
+            cooperative: "Cooperative",
+            autocritic: "Autocritic",
+            expertise: "Expertise",
+            projects: "Personal Projects",
+            mexico: "Mexico",
+            technologies: "Technologies",
+            education: "Education",
+            engineering: "Computer Systems Engineering",
+            technician: "Computer Equipment Support and Maintenance Technician",
+            languages: "Languages",
+            spanish: "Spanish:",
+            spanish_level : "Native",
+            english: "English:",
+            english_level : "Intermediate (B1)",
+            competitions: "Competitions"
+        },
+        exp : [
+            {
+                empresa: "STEFANINI S.A DE C.V",
+                imagen: "images/projects/Stefanini_Group_Logo.png",
+                descripcion: "Global company with 38 years of market experience, offering a robust selection of services such as financial services, automation, cloud, IoT and UX.",
+                contrato: "May - November 2025",
+                actividades: [
+                    "Collaborate with BANAMEX, creating installation and queue configuration scripts for IBM MQ, and developed Spring Boot microservices tailored to client requirements, thereby improving system integration.",
+                    "Participate as a QA Tester, ensuring software quality by executing automated tests using Postman and documenting the results in automated reports using the Karate Framework, Newman, and Node.js."
+                ]
+            },
+            {
+                empresa: "TASF S.A de C.V",
+                imagen: "images/projects/tasf.png",
+                descripcion: "Mexican company with 10 years of experience. Specialists in financial software development.",
+                contrato: "August 2022 - October 2024",
+                actividades: [
+                    "Develop a module capable of interpreting credit bureau data frames for the Technical Support area of the Software Development department, facilitating their readability and significantly reducing the time required for decision-making and error detection.",
+                    "Build SOAP WS and REST APIs using Java and Spring Boot.",
+                    "Design user interfaces for desktop applications using WPF and .NET, and implemented the developed WS and REST APIs.",
+                    "Develop views, SP's and complex queries using SQL Server.",
+                    "Design, develop, and generate PDF reports using Jaspersoft.",
+                    "Preventive and corrective maintenance for the functions of the web system's developed modules, adapting to the needs of each client."
+                ]
+            }
+        ],
+        projects : [
+            {
+                name: "API LIGA_MX",
+                hypervincule: "https://github.com/Tavote29/API_LigaMx",
+                description: "API that simulates the official Liga MX website, applying the technologies I have learned throughout my professional career.",
+                image: "images/projects/API_LigaMX.png",
+                text: "API based on the current Liga MX system, enabling CRUD operations and the management of statistics—such as league standings, top scorer tables, and more. Developed using Spring Boot 4, SQL Server as database, Spring Security for role-based access control with JWT authentication, Swagger for documentation, and JUnit with Mockito for unit testing."
+            },
+            {
+                name: "Mi Portfolio",
+                hypervincule: "https://github.com/Tavote29/Tavote29.github.io",
+                description: "My personal website. Where I showcase my professional career, skills, and the projects I have completed.",
+                image: "images/projects/portfolio.PNG",
+                text: "Personal website where you can find more details about my professional career. Built primarily with Bootstrap v3.3.4, using HTML5, CSS, and JavaScript."
+            },
+            {
+                name: "MINECODE",
+                hypervincule: "https://github.com/Tavote29/Compilador",
+                description: "Compiler for Java",
+                image: "images/projects/Minecode.png",
+                text: "A program developed as part of a school project, designed in C# using the Irony library, created for the Java language, and demonstrating lexical, syntactic, and semantic analysis"
+            }
+        ],
+        lenguages : [
+            {
+                lenguage: "Java (Springboot)",
+                logos: [
+                    "images/logos/java.png",
+                    "images/logos/spring-boot.png"   
+                ]
+            },
+            {
+                lenguage: "C# (.NET)",
+                logos:[
+                    "images/logos/csharp.png",
+                    "images/logos/NET-Framework-Symbol.png"
+                ]
+            },
+            {
+                lenguage: "SQL (SQL Server & MySQL)",
+                logos:[
+                    "images/logos/mysql.png",
+                    "images/logos/sqlserver.png"
+                ]
+            },
+            {
+                lenguage: "HTML5, CSS3 & JAVASCRIPT",
+                logos:[
+                    "images/logos/html.png",
+                    "images/logos/css.png",
+                    "images/logos/logo-javascript.png"
+                ]
+            },
+            {
+                lenguage: "PHP (Laravel)",
+                logos: [
+                    "images/logos/php.png",
+                    "images/logos/laravel.png"
+                ]
+            }
         ]
     }
-];
-
-const lenguages = [
-    {
-        lenguage: "Java (Springboot)",
-        logos: [
-            "images/logos/java.png",
-            "images/logos/spring-boot.png"   
-        ]
-    },
-    {
-        lenguage: "C# (.NET)",
-        logos:[
-            "images/logos/csharp.png",
-            "images/logos/NET-Framework-Symbol.png"
-        ]
-    },
-    {
-        lenguage: "SQL (SQL Server & MySQL)",
-        logos:[
-            "images/logos/mysql.png",
-            "images/logos/sqlserver.png"
-        ]
-    },
-    {
-        lenguage: "HTML5, CSS3 & JAVASCRIPT",
-        logos:[
-            "images/logos/html.png",
-            "images/logos/css.png",
-            "images/logos/logo-javascript.png"
-        ]
-    },
-    {
-        lenguage: "PHP (Laravel)",
-        logos: [
-            "images/logos/php.png",
-            "images/logos/laravel.png"
-        ]
-    }
-];
-
-const projects = [
-    {
-        name: "API LIGA_MX",
-        hypervincule: "https://github.com/Tavote29/API_LigaMx",
-        description: "Una API que simula al de la página oficial de la Liga MX aplicando las tecnologías que aprendí lo largo de mi carrera profesional",
-        image: "images/projects/API_LigaMX.png",
-        text: "Una API basado en el sistema actual de la liga MX en las que se puede hacer operaciones CRUD y manejo de estadísticas como tabla de posiciones, tabla de goleo, etc. Elaborado con Springboot 4, usando SQL Server como gestor de base de datos, Spring Security para manejo de roles con autenticación JWT, implementando Swagger para la documentación y JUnit con Mockito para la elaboración de pruebas unitarias."
-    },
-    {
-        name: "Mi Portfolio",
-        hypervincule: "https://github.com/Tavote29/Tavote29.github.io",
-        description: "Mi propia página web donde muestro mi carrera profesional, habilidades y proyectos que he realizado",
-        image: "images/projects/portfolio.PNG",
-        text: "Una página web personal en la que se puede encontrar más detalles de mi carrera profesional. Hecho principalmente con Bootstrap v3.3.4 con HTML 5, CSS y Javascript"
-    },
-    {
-        name: "MINECODE",
-        hypervincule: "https://github.com/Tavote29/Compilador",
-        description: "Un compilador para el lenguaje Java",
-        image: "images/projects/Minecode.png",
-        text: "Un programa que formó parte de un proyecto escolar, diseñado en C# con la librería Irony hecho para el lenguaje Java y muestre los analizadores léxico, sintáctico y semántico."
-    }
-];
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
+let currentLang = localStorage.getItem('lang') || 'es';
 const btnSwitch = document.querySelector('#switch');
+const switchLang = document.querySelector('#language_switch')
 
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('light');
-    
     btnSwitch.classList.toggle('active');
 });
 
@@ -10,54 +11,60 @@ function initTooltip(){
     $('[data-toggle="tooltip"]').tooltip();
 }
 
-$(document).ready(function(){
-    initTooltip();
-})
+function translateStaticText(){
+    const elements = document.querySelectorAll('[data-translate]')
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate')
+        element.textContent = translations[currentLang].static[key]
+    })
+}
 
-const container = document.getElementById("exp-container");
+function renderExperience(expData){
+    const container = document.getElementById("exp-container");
+    container.innerHTML = ''
+    expData.forEach(exp => {
+        const item = document.createElement('div');
+        item.className = 'item row';
 
-exp.forEach(exp => {
-    const item = document.createElement('div');
-    item.className = 'item row';
+        item.innerHTML = `
+            <a class="col-md-4 col-sm-4 col-xs-12" >
+            <img class="img-responsive project-image" src="${exp.imagen}" />
+            </a>
 
-    item.innerHTML = `
-        <a class="col-md-4 col-sm-4 col-xs-12" >
-        <img class="img-responsive project-image" src="${exp.imagen}" />
-        </a>
+            <div class="desc col-md-8 col-sm-8 col-xs-12">
+                <h3 class="title">${exp.empresa}</h3>
+                <p class="summary">${exp.contrato}<p>
 
-    <div class="desc col-md-8 col-sm-8 col-xs-12">
-      <h3 class="title">${exp.empresa}</h3>
-      <p class="summary">${exp.contrato}<p>
-
-      <p class="summary">${exp.descripcion}</p>
-      <div class="desc text-left">
-        <ul>
-          ${exp.actividades.map(act => `<li>${act}</li>`).join('')}
-        </ul>
-      </div>
-    </div>   
-    `
+                <p class="summary">${exp.descripcion}</p>
+                <div class="desc text-left">
+                    <ul>${exp.actividades.map(act => `<li>${act}</li>`).join('')}</ul>
+                </div>
+            </div>   
+        `
     container.appendChild(item);
 });
+}
 
-const stack = document.getElementById("technologic-stack");
-lenguages.forEach(dat => {
-    const item = document.createElement('div');
-    item.className= 'item';
+function renderStack(technologies){
+    const stack = document.getElementById("technologic-stack");
+    stack.innerHTML = ''
+    technologies.forEach(dat => {
+        const item = document.createElement('div');
+        item.className= 'item';
 
-    item.innerHTML = `
-        <div class="item">
+        item.innerHTML = `
+            <div class="item">
             <h3 class="level-title">${dat.lenguage}<span class="level-label" data-toggle="tooltip" data-placement="left" data-animation="true" title="You can use the tooltip to add more info..."></span></h3>
-             ${dat.logos.map(logo => 
-            `<img class="logos" src="${logo}" alt="${exp.lenguage}">`
-        ).join('')}
-    `
-
+            ${dat.logos.map(logo => 
+                `<img class="logos" src="${logo}" alt="${dat.lenguage}">`
+            ).join('')}
+        `
     stack.appendChild(item);
-})
+    })
+}
 
-document.addEventListener("DOMContentLoaded", () => {
 
+function renderProjects(projects){
     const carousel = document.getElementById("carousel_elements");
     const indicators = document.querySelector('#carrusel .carousel-indicators');
 
@@ -106,6 +113,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     indicators.appendChild(li);
     });
+    $('#carrusel').carousel();
+}
 
+function renderPage(){
+    const data = translations[currentLang]
+    translateStaticText()
+    renderExperience(data.exp)
+    renderStack(data.lenguages)
+    renderProjects(data.projects)
+    initTooltip()
+}
 
+function changeLanguage(lang){
+    currentLang = lang;
+    localStorage.setItem('lang', lang);
+    renderPage();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderPage()
 })
+
+
